01ff35c3900724e7e4da3d65c8da673f53a8d531
maturu <maturu.410@gmail.com>
2021-04-20 23:41:43 +0900

    react router tutorial

---
+++ src/js/pages/Layout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+
+class Layout extends React.Component {
+  navigate() {
+    console.log(this.props.history);
+    this.props.history.push("/");
+  }
+  render() {
+    return (
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="/archives/some-other-articles?date=yesterday&filter=none" className="btn btn-warning">archives (some other articles)</Link>
+        <Link to="/archives?date=today&filter=hot" className="btn btn-danger">archives</Link>
+        <NavLink to="/settings/main" className="btn btn-success" activeClassName="btn-danger">settings</NavLink>
+        <Link to="/settings/extra" className="btn btn-success">settings (extra)</Link>
+        <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Layout);
--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,26 @@ module.exports = {
   module: {
     rules: [{
       test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
-          }
-        }]
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          plugins: ['react-html-attrs'],
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
       }]
-    },
-    output: {
-      path: __dirname + "/src/",
-      filename: "client.min.js"
-    },
-    plugins: debug ? [] : [
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    ]
+    }]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js",
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
